9a3da7cd42b67b0a8d34ac5c5384c1c03959664c
ngbedebarnabas <ngbedebarnabas@gmail.com>
2026-01-27 10:58:17 +0100

    Completed Fake Authentication

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -54,6 +54,8 @@ function useAuth() {
   const context = useContext(AuthContext);
   if (context === undefined)
     throw new Error("AuthContext was used outside of AuthProvider");
+
+  return context;
 }
 
 export { AuthProvider, useAuth };
